01ef974076953f7293505b9bf47985eab65a23c9
Vladimir <destroyer.hramov@yandex.ru>
2023-11-17 01:07:46 +0300

    resize content

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import {state} from "./utils/state.js";
 
+const body = document.querySelector('#body');
 const soundImg = document.getElementById('game__sound_img');
 const canvas = document.getElementById('root');
 const ctx = canvas.getContext('2d');
@@ -20,8 +21,15 @@ function render() {
     ctx.fillStyle = "white";
     ctx.strokeStyle = "black";
     ctx.textAlign = "center";
-    ctx.fillText("ЛОВИ ИХ!", canvas.width/2, canvas.height/2, );
-    ctx.strokeText("ЛОВИ ИХ!", canvas.width/2, canvas.height/2, );
+    ctx.fillText("ЛОВИ ИХ!", canvas.width/2, canvas.height/3.5);
+    ctx.strokeText("ЛОВИ ИХ!", canvas.width/2, canvas.height/3.5);
 }
 
 init();
+
+window.addEventListener('resize', () => {
+    if(document.documentElement.clientWidth > 600) canvas.width = 600;
+    else canvas.width = document.documentElement.clientWidth;
+
+    canvas.height = document.documentElement.clientHeight;
+})
